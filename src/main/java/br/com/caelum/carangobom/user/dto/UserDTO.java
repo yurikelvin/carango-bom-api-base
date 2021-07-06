79d4c44fc25dd14dc99300e04c8a4d9b8c42f4a3
luan <luancmadev@gmail.com>
2021-07-06 12:24:09 -0300

    Created the UserDTO

---
+++ src/main/java/br/com/caelum/carangobom/user/dto/UserDTO.java
@@ -0,0 +1,23 @@
+package br.com.caelum.carangobom.user.dto;
+
+import br.com.caelum.carangobom.user.model.User;
+
+public class UserDTO {
+
+    private String username;
+    private String password;
+
+    public UserDTO(User user) {
+        super();
+        this.username = user.getUsername();
+        this.password = user.getPassword();
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
