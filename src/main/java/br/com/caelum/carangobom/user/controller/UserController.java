5cef6058544d4926528ff4da8fd63f54594072d2
luan <luancmadev@gmail.com>
2021-07-06 19:17:42 -0300

    Created the UserRepository and UserController tests

--- src/main/java/br/com/caelum/carangobom/user/controller/UserController.java
+++ src/main/java/br/com/caelum/carangobom/user/controller/UserController.java
@@ -1,9 +1,20 @@
 package br.com.caelum.carangobom.user.controller;
 
+import br.com.caelum.carangobom.user.dto.UserDTO;
+import br.com.caelum.carangobom.user.form.UserForm;
+import br.com.caelum.carangobom.user.model.User;
 import br.com.caelum.carangobom.user.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import javax.transaction.Transactional;
+import javax.validation.Valid;
+import java.net.URI;
 
 @RestController
 public class UserController {
@@ -12,5 +23,15 @@ public class UserController {
     private UserRepository userRepository;
 
 
+    @RequestMapping("/users")
+    @PostMapping
+    @Transactional
+    public ResponseEntity<UserDTO> create(@RequestBody @Valid UserForm userForm, UriComponentsBuilder uriBuilder){
+        User user = userForm.convert();
 
+        userRepository.save(user);
+
+        URI uri = uriBuilder.path("/users/{id}").buildAndExpand(user.getId()).toUri();
+        return ResponseEntity.created(uri).body(new UserDTO(user));
+    }
 }
--- src/main/java/br/com/caelum/carangobom/user/repository/UserRepository.java
+++ src/main/java/br/com/caelum/carangobom/user/repository/UserRepository.java
@@ -4,4 +4,6 @@ import br.com.caelum.carangobom.user.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findByUsername(String username);
 }
