ec6ac77ac9cdeef76928763c3cff87f2507bafb6
luan <luancmadev@gmail.com>
2021-07-12 15:55:03 -0300

    Created the service to create user

---
+++ src/main/java/br/com/caelum/carangobom/user/CreateUserService.java
@@ -0,0 +1,34 @@
+package br.com.caelum.carangobom.user;
+
+import br.com.caelum.carangobom.exception.BadRequestException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+
+public class CreateUserService {
+
+    @Autowired
+    private final UserRepository repository;
+
+    public CreateUserService(UserRepository repository) {
+        this.repository = repository;
+    }
+
+    public  ResponseEntity<UserDTO> createNewUser(User user, UriComponentsBuilder uriBuilder){
+
+        // TODO - Create a service to validate user
+        User isCreated = repository.findByUsername(user.getUsername());
+
+        if (isCreated != null) {
+            String errorMessage = "Usuário já cadastrado";
+            throw  new BadRequestException(errorMessage);
+        }
+
+        repository.save(user);
+        URI uri = uriBuilder.path("/users/{id}").buildAndExpand(user.getId()).toUri();
+        return ResponseEntity.created(uri).body(new UserDTO(user));
+
+    }
+}
