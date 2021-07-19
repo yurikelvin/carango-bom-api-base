7c48f8c99ff197fadf138ac893d028302f5e020f
luan <luancmadev@gmail.com>
2021-07-19 15:30:02 -0300

    refactoring the profile

--- src/main/java/br/com/caelum/carangobom/auth/Profile.java
+++ src/main/java/br/com/caelum/carangobom/auth/Profile.java
@@ -1,5 +1,7 @@
 package br.com.caelum.carangobom.auth;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import org.springframework.security.core.GrantedAuthority;
 
 import javax.persistence.Entity;
@@ -8,6 +10,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
+@Data
+@AllArgsConstructor
 public class Profile implements GrantedAuthority {
 
     @Id
@@ -15,22 +19,6 @@ public class Profile implements GrantedAuthority {
     private Long id;
 
     private String name;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 
     @Override
     public String getAuthority() {
