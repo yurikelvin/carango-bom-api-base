191789bd6bfc71e71ccbbb28b44ddc0e7ec2b146
luan <luancmadev@gmail.com>
2021-07-19 14:28:14 -0300

    testing the user-entity

---
+++ src/test/java/br/com/caelum/carangobom/user/UserEntityTest.java
@@ -0,0 +1,31 @@
+package br.com.caelum.carangobom.user;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class UserEntityTest {
+
+    @Test
+    public void createNewUserWithConstructorWithIdPasswordAndUsername(){
+        String username = "user";
+        String password = "password";
+        Long id = 1L;
+
+        User user = new User(id, username, password);
+
+        Assert.assertTrue(user.isEnabled());
+        Assert.assertTrue(user.isAccountNonExpired());
+        Assert.assertTrue(user.isAccountNonLocked());
+        Assert.assertTrue(user.isCredentialsNonExpired());
+        Assert.assertEquals(user.getUsername(), username);
+        Assert.assertEquals(user.getPassword(), password);
+        Assert.assertEquals(user.getId(), id);
+    }
+
+    @Test
+    public void creatingWithConstructorUserAndPassword(){
+        String username = "user";
+        String password = "password";
+        User user = new User(username, password);
+    }
+}
