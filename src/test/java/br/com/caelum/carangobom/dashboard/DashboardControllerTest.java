f2f6067967a47e9f0bb99408441f0f5b534f74c2
luan <luancmadev@gmail.com>
2021-07-20 16:23:23 -0300

    created the integration test

--- src/test/java/br/com/caelum/carangobom/dashboard/DashboardControllerTest.java
+++ src/test/java/br/com/caelum/carangobom/dashboard/DashboardControllerTest.java
@@ -8,16 +8,18 @@ import org.junit.jupiter.api.BeforeEach;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ActiveProfiles;
 
+import javax.management.Query;
+import javax.persistence.EntityManager;
 import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
-import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 import static org.mockito.MockitoAnnotations.openMocks;
 
 @ActiveProfiles("test")
@@ -27,27 +29,16 @@ public class DashboardControllerTest {
     @Mock
     private VehicleRepository vehicleRepository;
 
-    @Mock
     private DashboardController dashboardController;
 
     @BeforeEach
     public void mockConfig() {
         openMocks(this);
+        dashboardController = new DashboardController(vehicleRepository);
     }
 
     @Test
     public void shouldConvertTheListReturnedList(){
-        List<List<Object>> dashboardList =
-                List.of(
-                        List.of("Brand 1", new BigInteger(String.valueOf(2)), new BigDecimal(2000000)),
-                        List.of("Brand 2", new BigInteger(String.valueOf(3)), new BigDecimal(54000000))
-                );
-        List<Dashboard> dashboardAction = Dashboard.toDashboard(dashboardList);
-        Assert.assertEquals(dashboardAction.size(), dashboardList.size());
-    }
-
-    @Test
-    public void shouldListTheListOfBrands(){
         List<List<Object>> dashboardList =
                 List.of(
                         List.of("Brand 1", new BigInteger(String.valueOf(2)), new BigDecimal(2000000)),
---
+++ src/test/java/br/com/caelum/carangobom/dashboard/DashboardIntegrationTest.java
@@ -0,0 +1,44 @@
+package br.com.caelum.carangobom.dashboard;
+
+
+import br.com.caelum.carangobom.user.UserRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import javax.persistence.EntityManager;
+import java.net.URI;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+public class DashboardIntegrationTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private UserRepository repository;
+
+    @Autowired
+    private EntityManager entityManager;
+
+    @Test
+    public void shouldReturn200WhenGetTheListOfUser() throws Exception {
+        URI uri = new URI("/dashboard");
+        mockMvc.perform(MockMvcRequestBuilders
+                .get(uri)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+    }
+
+}
